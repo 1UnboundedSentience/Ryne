609a3a8a978a2f93b194b7ee0d3ad1ce43aa49ac
1UnboundedSentience <jsnx21@gmail.com>
2015-09-19 15:38:33 -0700

    initing new branch photo upload with photo model and controller

--- Ryne/config/routes.rb
+++ Ryne/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
 
+  # resource :user do
+  # end
+  resource :photos
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root to: "home#index"
--- Ryne/db/schema.rb
+++ Ryne/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919212634) do
+ActiveRecord::Schema.define(version: 20150919221743) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "geolocation"
+    t.string   "emotion"
+    t.string   "season"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
