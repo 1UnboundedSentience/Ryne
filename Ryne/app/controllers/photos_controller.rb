7d2dc504fb609d39a86f95f811d652112bea9c77
1UnboundedSentience <jsnx21@gmail.com>
2015-09-19 15:48:07 -0700

    new branch

--- Ryne/app/controllers/photos_controller.rb
+++ Ryne/app/controllers/photos_controller.rb
@@ -1,3 +1,12 @@
 class PhotosController < ApplicationController
-  belongs_to :user
+  def create
+    @article = Article.find(params[:article_id])
+    @photos = @photos.create(photos_params)
+    redirect_to article_path(@article)
+  end
+
+  private
+    def photos_params
+      params.require(:photos).permit(:commenter, :body)
+    end
 end
